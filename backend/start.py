654de799a1f140d19039f34be0eace56c791e8ac
42712 <115371094+42712@users.noreply.github.com>
2026-05-21 07:04:41 -0300

    Delete backend directory

--- backend/start.py
+++
@@ -1,7 +0,0 @@
-import os
-from server import app
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    print(f"🚀 Painel Aviator Iniciado - Porta {port}")
-    app.run(host="0.0.0.0", port=port, debug=False, threaded=True)
